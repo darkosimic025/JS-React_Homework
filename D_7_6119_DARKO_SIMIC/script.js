e4e8aa4abf3af75e30a385145643880b49a014e8
darkosimic025 <darkosimic025@gmail.com>
2021-12-15 16:08:45 +0100

    Push domaci 7

---
+++ D_7_6119_DARKO_SIMIC/script.js
@@ -0,0 +1,50 @@
+// 1.Zadatak_______________________________________________________________________________________________
+
+function mnozi(n, m) {
+    let proizvod=1;
+    for (n; n <= m; n++) {
+        proizvod = proizvod * n;
+    }
+    console.log(proizvod);
+}
+
+mnozi(1, 3);
+
+
+// 2.Zadatak_________________________________________________________________________________________________
+
+function arSr(n, m) {
+    let suma = 0;
+    brojBrojeva = 0;
+    for (n; n <= m; n++) {
+        suma = suma + n;
+        brojBrojeva++; 
+    } aritmetickaSredina = suma / brojBrojeva;
+    console.log(aritmetickaSredina);
+}
+arSr(0, 10);
+
+// 3.Zadatak_____________________________________________________________________________________________________
+
+function arSr3(n, m) {
+    let suma = 0;
+    brojBrojeva = 0;
+   
+    for (n; n <= m; n++) {
+        if (n % 10 == 3) {
+            suma = suma + n;
+            brojBrojeva++;
+        }
+    }
+    aritmetickaSredina = suma / brojBrojeva;
+    console.log(aritmetickaSredina);
+}
+arSr3(0, 13);
+
+// 4.Zadatak_________________________________________________________________________________________________________
+
+function tekst(velicina) {
+    document.body.innerHTML += `<p style="font-size: ${velicina}px;"> Tekst paragrafa </p>`;
+}
+
+tekst(40);
